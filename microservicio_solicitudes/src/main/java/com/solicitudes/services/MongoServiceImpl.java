329baf450a0e16c403fa97178103f4a6fa7cf8c0
HILDEBOG\calderonkl <calderonkl@globalhitss.com>
2021-11-16 15:35:40 -0500

    prueba 10

--- microservicio_solicitudes/src/main/java/com/solicitudes/services/MongoServiceImpl.java
+++ microservicio_solicitudes/src/main/java/com/solicitudes/services/MongoServiceImpl.java
@@ -1,8 +1,12 @@
 package com.solicitudes.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
 import com.excepciones.GeneralException;
@@ -16,6 +20,13 @@ public class MongoServiceImpl implements IMongoService {
 
 	@Autowired
 	IDocumentoDao documentoDao;
+
+	private final MongoTemplate mongoTemplate;
+
+	@Autowired
+	public MongoServiceImpl(MongoTemplate mongoTemplate) {
+		this.mongoTemplate = mongoTemplate;
+	}
 
 	@Override
 	public String crearDocumento(String cedula, String historiaClinica, String nombreCliente) throws Exception {
@@ -60,15 +71,21 @@ public class MongoServiceImpl implements IMongoService {
 	public Documento getDocumentoPorId(String id) throws Exception {
 		Documento documento = new Documento();
 		try {
-			Optional<Documento> documentoOptional = documentoDao.findById(id);
-
-			if (documentoOptional == null || documentoOptional.isEmpty()) {
-				throw GeneralException.throwException(this, new Exception(), "No hay documentos asociados al id", "VD01");
+			Query query = new Query();
+			query.addCriteria(Criteria.where("_id").is(id));
+			List<Documento> documentos = mongoTemplate.find(query, Documento.class);
+			if (documentos != null && documentos.size() > 0) {
+				documento = documentos.get(0);
 			}
-			else {
-				documento.setId(documentoOptional.get().getId());
-				documento.setCedula(documentoOptional.get().getCedula());
-			}
+//			Optional<Documento> documentoOptional = documentoDao.findById(id);
+//
+//			if (documentoOptional == null || documentoOptional.isEmpty()) {
+//				throw GeneralException.throwException(this, new Exception(), "No hay documentos asociados al id",
+//						"VD01");
+//			} else {
+//				documento.setId(documentoOptional.get().getId());
+//				documento.setCedula(documentoOptional.get().getCedula());
+//			}
 
 		} catch (Exception ex) {
 			if (ex instanceof MongoSecurityException) {
